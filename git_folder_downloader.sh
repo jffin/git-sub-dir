19e5047253b3ffe243a70d40ce01d4d10c2ef1af
jffin <22579497+jffin@users.noreply.github.com>
2020-01-12 17:58:50 +0200

    bug fix

--- git_folder_downloader.sh
+++ git_folder_downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-  
+
 # usage
 # ./git_folder_downloader.sh [[ https://api.github.com/repos/:owner/:repo/contents/:path ]] [[ destination folder ]] [[ directory to remove while following path ]]
 
@@ -8,6 +8,7 @@ directory="$2"
 follow_structure="$3"
 
 download_file() {
+        printf ${3}${4}${1}"\n"
         mkdir -p ${3}${4}
         curl -s -o ${3}${4}${1} ${2}
 }
@@ -22,16 +23,18 @@ loop_json() {
                 file_name=$(_jq '.name')
                 path="/"
                 download_url=$(_jq '.download_url')
-
+                
                 if [[ ! -z ${follow_structure} ]]; then
                         path=$(_jq '.path')
                         path=${path//${follow_structure}/}
                         path=${path//${file_name}/}
+                        printf ${path}"\n"
                 fi
 
                 if [ ${download_url} = ${null_url} ]; then
+                        printf ${url}
                         folder_json=$(curl -L ${url})
-                        loop_json ${forlder_json}
+                        loop_json "${folder_json}"
                 else
                         download_file ${file_name} ${download_url} ${directory} ${path}
                 fi
